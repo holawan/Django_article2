f094504da47f2408d82a9eb7be4e539aa660c91e
holawan <asdf134652@gmail.com>
2022-04-08 14:17:36 +0900

    허옹 HTTP Method 설정

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
 from movies.forms import MovieForm
 from .models import Movie
 # Create your views here.
+
+
+@require_safe
 def index(request) :
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by('-pk')
     context = {
         'movies':movies
     }
@@ -12,7 +15,7 @@ def index(request) :
 
 
 #Create html 만들기 
-
+@require_http_methods(['GET','POST'])
 def create(request) :
     if request.method == 'POST' :
         form = MovieForm(request.POST)
@@ -28,17 +31,19 @@ def create(request) :
 
     return render(request, 'movies/create.html',context)
 
-#Detail 만들기
+@require_safe
 def detail(request,pk) :
-    movie = Movie.objects.get(pk=pk) 
+    movie = get_object_or_404(Movie,pk=pk)
 
     context = {
         'movie' :movie
     }
     return render(request,'movies/detail.html',context)
 
+    
+@require_http_methods(['GET','POST'])
 def update(request,pk) :
-    movie = Movie.objects.get(pk=pk)
+    movie = get_object_or_404(Movie,pk=pk)
     if request.method == 'POST' :
         form = MovieForm(request.POST,instance=movie)
 
@@ -54,10 +59,11 @@ def update(request,pk) :
 
     return render(request, 'movies/update.html',context)
 
+@require_POST
 def delete(request,pk):
     if request.method == 'POST' :
         #POST형태로 해당 영화의 Primary key가 전달되면  
-        movie = Movie.objects.get(pk=pk)
+        movie = get_object_or_404(Movie,pk=pk)
         #게시글을 삭제한다. 
         movie.delete()
     return redirect('movies:index')

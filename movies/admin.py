c488f443a095874704efd8dbc637cd1b46dbf995
holawan <asdf134652@gmail.com>
2022-04-08 12:47:55 +0900

    adminpage

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-
+from .models import Movie
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin) :
+    # admin 페이지에서는 영화 기본 정보 보여주기 
+    list_display = ('title','genre','audience','score')
+
+admin.site.register(Movie,MovieAdmin)

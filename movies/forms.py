63f5138e4338723b70f8eb6bf8f1c7bc590a68ab
holawan <asdf134652@gmail.com>
2022-04-08 14:37:26 +0900

    choice_field

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,24 @@
 from django import forms
 from .models import Movie
 
-class MovieForm(forms.ModelForm) :
 
+
+
+class MovieForm(forms.ModelForm) :
+    GENRE_A = 'comedy'
+    GENRE_B = 'horror'
+    GENRE_C = 'romance'
+
+    GENRE_CHOICES = [
+        (GENRE_A,'코미디'),
+        (GENRE_B,'공포'),
+        (GENRE_C,'로맨스')
+    ] 
+
+    genre = forms.ChoiceField(
+        choices=GENRE_CHOICES,
+        widget=forms.Select()
+    )
     class Meta :
         model = Movie 
 
